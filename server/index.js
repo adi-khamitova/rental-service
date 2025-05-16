e8934e41649b10102bab29212677769f07c0d148
Adelya Khamitova <adi.khamitova@gmail.com>
2025-05-16 17:59:10 +0300

    lab2

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import sequelize from './config/database.js';
+import cors from 'cors';
+import router from './routes/index.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5051;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
+
+app.get('/', (req, res)=> {
+    res.status(200).json({message: 'Ура работает'})
+});
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+
+        app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
+
